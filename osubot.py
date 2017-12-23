0b8df36ec235b65a4023571cf820c928781cf230
151250063 <151250063@smail.nju.edu.cn>
2017-12-24 03:12:16 +0900

    助眠

--- osubot.py
+++ osubot.py
@@ -2,6 +2,11 @@ from group_rank import *
 from caculate_pp import *
 import random
 
+help_message="!rank [0123] 查看群内各模式菜鸡排名\n" \
+             "!capp 查询单图pp\n" \
+             "!roll 1-100随机roll点\n" \
+             "!sleep [n] 让bot口球你n秒"
+
 def onQQMessage(bot, contact, member, content):
     if contact.ctype=='group' and (contact.qq=='203341856' or contact.qq=='200064826'):
         if content.startswith('!'):
@@ -27,5 +32,13 @@ def onQQMessage(bot, contact, member, content):
             elif command.startswith("roll"):
                 bot.SendTo(contact, member.name+" 掷出 "+str(random.randint(1, 100))+" 点")
             elif command.startswith("help"):
-                bot.SendTo(contact, "!rank [0123] 查看群内各模式菜鸡排名\n!capp 查询单图pp\n!roll 1-100随机roll点")
+                bot.SendTo(contact, help_message)
+            elif command.startswith("sleep"):
+                second = int(content[7:])
+                gl = bot.List('group', contact.name)
+                if gl:
+                    group = gl[0]
+                    membs = bot.List(group, member.name)
+                    if membs:
+                        bot.GroupShut(group, membs, second)
 
